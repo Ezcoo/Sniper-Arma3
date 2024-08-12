3c91b4fc1434a4479fb804de4cd7a49370491774
Ezcoo <4085301+Ezcoo@users.noreply.github.com>
2024-08-12 23:36:34 +0300

    Add & update sound files in CfgSounds

--- Sniper.VR/rsc/CfgSounds.hpp
+++ Sniper.VR/rsc/CfgSounds.hpp
@@ -2,9 +2,27 @@ class CfgSounds
 {
 	sounds[] = {}; // OFP required it filled, now it can be empty or absent depending on the game's version
 
-	class notification1
+	class killNotification
 	{
 		name = "Killfeed notification";						// display name
 		sound[] = { "rsc\common\sfx\notification_1.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
+	};
+
+    class loomingDanger
+	{
+		name = "Looming danger";						// display name
+		sound[] = { "rsc\common\sfx\loomingDanger.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
+	};
+
+    class loomingDanger2
+	{
+		name = "Looming danger 2";						// display name
+		sound[] = { "rsc\common\sfx\loomingDanger2.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
+	};
+
+    class loomingDanger3
+	{
+		name = "Looming danger 3";						// display name
+		sound[] = { "rsc\common\sfx\loomingDanger3.ogg", 1, 1, 100 };	// file, volume, pitch, maxDistance
 	};
 };
